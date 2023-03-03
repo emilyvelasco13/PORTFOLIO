d7340359a1e874d21356de85d3d3dfdfd854d0cb
SC- Emily R. Velasco <126373226+emilyvelasco13@users.noreply.github.com>
2023-03-03 11:46:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+ <script type="text/javascript" src="js/JQuery3.3.1.js"></script>
+	<script type="text/javascript">
+	
+		  /*Responsive Navigation*/
+	  $(document).ready(function(){
+	 $('.toggle').click(function(){
+		 $('.toggle').toggleClass('active')
+		 
+		 $('nav ul').toggleClass('active-menu')
+		
+		 
+	 })
+ });
+ 
+	</script>
+  
